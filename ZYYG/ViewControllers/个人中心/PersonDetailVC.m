9f231bb209f1f151c91bebf27ef1ea598f920b5b
emccchina <wuhaizeng@emcc.net.cn>
2014-12-15 08:29:53 +0800

    个人详情

--- ZYYG/ViewControllers/个人中心/PersonDetailVC.m
+++ ZYYG/ViewControllers/个人中心/PersonDetailVC.m
@@ -11,9 +11,13 @@
 #import "EditPersonVC.h"
 
 @interface PersonDetailVC ()
+<UITextFieldDelegate>
 {
     NSMutableArray *detailArry;
     UserInfo *user;
+    NSArray *titles;
+    NSArray *placeholders;//占位符
+    NSMutableArray *infos;//个人信息说明
 }
 
 @end
@@ -23,11 +27,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self showBackItem];
+    titles = @[@"昵称", @"真实姓名", @"性别", @"年收入", @"手机号", @"邮箱", @"所在地", @"详细地址", @"邮编"];
+    placeholders = @[@"请输入昵称", @"请输入真实姓名", @"请选择性别", @"请输入年收入", @"请输入手机号", @"请输入邮箱", @"请输入所在地", @"请输入详细地址", @"请输入邮编"];
+    
     user =[UserInfo shareUserInfo];
+    infos = [[NSMutableArray alloc] init];
+    [self setInfos];
     self.personDetailTableView.delegate = self;
     self.personDetailTableView.dataSource = self;
     
     // Do any additional setup after loading the view.
+}
+
+- (void)setInfos
+{
+    [infos addObject:user.nickName ? :@""];
+    [infos addObject:user.realName ? :@""];
+    [infos addObject:user.gender ? :@""];
+    [infos addObject:user.income ? :@""];
+    [infos addObject:user.mobile ? :@""];
+    [infos addObject:user.email ? :@""];
+    [infos addObject:user.provideCode ? :@""];
+    [infos addObject:user.detailAddress ? :@""];
+    [infos addObject:user.zipCode ? :@""];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -40,7 +62,7 @@
 }
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10;
+    return titles.count;
 }
 -(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
@@ -48,68 +70,15 @@
 }
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:@"personDetail"];
-    UITableViewCell *imageCell=[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"imageCell"];
-    switch (indexPath.row) {
-        case 0:
-            if(user.headImage &&![@"" isEqualToString:user.headImage]){
-                [imageCell.imageView setImageWithURL:[NSURL URLWithString:user.headImage]];
-            }else{
-                [imageCell.imageView setImage:[UIImage imageNamed:@"avatar.png"]];
-            }
-            imageCell.textLabel.text=@"头像";
-            imageCell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            return imageCell;
-            break;
-        case 1:
-            cell.textLabel.text=@"昵称";
-            cell.detailTextLabel.text=user.nickName;
-            
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 2:
-            cell.textLabel.text=@"真实姓名";
-            cell.detailTextLabel.text=user.realName;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 3:
-            cell.textLabel.text=@"性别";
-            cell.detailTextLabel.text=([user.gender isEqual:@"0" ]? @"女":@"男");
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 4:
-            cell.textLabel.text=@"年收入";
-            cell.detailTextLabel.text=user.income;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 5:
-            cell.textLabel.text=@"手机";
-            cell.detailTextLabel.text=user.mobile;
-
-            break;
-        case 6:
-            cell.textLabel.text=@"邮箱";
-            cell.detailTextLabel.text=user.email;
-            
-            break;
-        case 7:
-            cell.textLabel.text=@"所在地";
-            cell.detailTextLabel.text=[NSString stringWithFormat:@"%@+%@+%@" ,user.provideCode,user.cityCode,user.aeraCode];
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 8:
-            cell.textLabel.text=@"详细地址";
-            cell.detailTextLabel.text=user.detailAddress;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        case 9:
-            cell.textLabel.text=@"邮编";
-            cell.detailTextLabel.text=user.zipCode;
-            cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
-            break;
-        default:
-            break;
-    }
+    UITableViewCell *cell=[tableView dequeueReusableCellWithIdentifier:@"PersonDetailCell" forIndexPath:indexPath];
+    UILabel *label = (UILabel*)[cell viewWithTag:1];
+    label.text = titles[indexPath.row];
+    UITextField *field = (UITextField*)[cell viewWithTag:2];
+    field.placeholder = placeholders[indexPath.row];
+    field.delegate = self;
+    field.text = infos[indexPath.row];
+    cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
 
@@ -117,66 +86,34 @@
 {
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    switch (indexPath.row) {
-        case 0:
-            [self presentDetailVC:@"头像" val:@"头像"];
-            break;
-        case 1:
-            [self presentDetailVC:@"昵称" val:user.nickName];
-            break;
-        case 2:
-            [self presentDetailVC:@"真实姓名" val:user.realName];
-            break;
-        case 3:
-            [self presentDetailVC:@"性别" val:([user.gender isEqual:@"0" ]? @"女":@"男")];
-            break;
-        case 4:
-            [self presentDetailVC:@"年收入" val:user.income];
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            [self presentDetailVC:@"所在地" val:[NSString stringWithFormat:@"%@+%@+%@" ,user.provideCode,user.cityCode,user.aeraCode]];
-            break;
-        case 8:
-            [self presentDetailVC:@"详细地址" val:user.detailAddress];
-            break;
-        case 9:
-            [self presentDetailVC:@"邮编" val:user.zipCode];
-            break;
-        default:
-            break;
-    }
-    
 }
-- (void)presentDetailVC:(id)editype  val:(id)textValue
+#pragma mark -textField
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField
 {
-    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"PersonCenterStoryboard" bundle:nil];
-    UIViewController* editVC = [storyboard instantiateViewControllerWithIdentifier:@"EditPersonVC"];
-    if ([(EditPersonVC *)editVC respondsToSelector:@selector(setEditType:)]) {
-        [editVC setValue:editype forKey:@"editType"];
-    }
-    if ([(EditPersonVC *)editVC respondsToSelector:@selector(setTextValue:)]) {
-        [editVC setValue:textValue forKey:@"textValue"];
-    }
-    editVC.hidesBottomBarWhenPushed = YES;
-    [self.navigationController pushViewController:editVC animated:YES];
+    [textField resignFirstResponder];
+    NSLog(@"fffffff");
+    return YES;
 }
 
-
-
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+-(void)textFieldDidBeginEditing:(UITextField *)textField
+{
+    NSLog(@"bbbbb");
+//    selectedField = textField;
+    CGPoint point = [textField convertPoint:textField.frame.origin toView:self.view];
+    //    NSLog(@"super view frame %@", NSStringFromCGPoint(point));
+    CGFloat height = kScreenHeight;
+    CGFloat space = (height - point.y - 160) - 250;
+    if (space < 0) {
+        CGPoint offset = self.personDetailTableView.contentOffset;
+        offset.y -= space;
+        [self.personDetailTableView setContentOffset:offset];
+    }
 }
-*/
+-(void)textFieldDidEndEditing:(UITextField *)textField
+{
+    NSLog(@"nnnnnn");
+    [self.personDetailTableView setContentOffset:CGPointZero];
+}
 
 @end
