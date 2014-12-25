7c99f89ef05f7ad8fd497806cda8cb8646d4496b
emccchina <topblby@163.com>
2014-12-25 15:40:19 +0800

    更新定制

--- ZYYG/ViewControllers/更多/CustomVC.m
+++ ZYYG/ViewControllers/更多/CustomVC.m
@@ -25,7 +25,6 @@
     
     self.customTableVIew.delegate=self;
     self.customTableVIew.dataSource=self;
-    self.customTableVIew.backgroundColor = kRedColor;
     self.title = @"私人定制";
     NSLog(@"私人定制");
 }
@@ -72,6 +71,17 @@
     return 60;
 }
 
+-(NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section
+{
+    if(section ==0){
+        return @"定制方法一:";
+    }else if (section ==1) {
+        return @"定制方法二:";
+    }else if (section ==2) {
+        return @"销售经理人列表:";
+    }
+    return @"";
+}
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (indexPath.section == 0) {
@@ -83,10 +93,57 @@
         [scrollCell reloadScrollViewData];
        
         return scrollCell;
-    }else if (indexPath.section==1 ||indexPath.section==2) {
+    }else if (indexPath.section==1) {
         SpecialDiscuss *sCell = (SpecialDiscuss *)[tableView dequeueReusableCellWithIdentifier:@"SpecialDiscuss" forIndexPath:indexPath];
+        [sCell.L1Image setImage:[UIImage imageNamed:@"custom1"]];
+        [sCell.L2Image setImage:[UIImage imageNamed:@"custom2"]];
+        [sCell.L3Image setImage:[UIImage imageNamed:@"custom3"]];
+        [sCell.L4Image setImage:[UIImage imageNamed:@"custom4"]];
+        
+        sCell.tit1.text=@"定制需求";
+        sCell.lab1.numberOfLines=0;
+        sCell.lab1.text=@"通过销售经理指定平台的签约艺术家,提出作品需求并支付创作定金.";
+        sCell.tit2.text=@"需求确认";
+        sCell.lab2.numberOfLines=0;
+        sCell.lab2.text=@"销售经理人与艺术家确认创作意向,确定创作风格,材质规格等创作细节.";
+        sCell.tit3.text=@"定制化创作";
+        sCell.lab3.numberOfLines=0;
+        sCell.lab3.text=@"根据客户需求,艺术家着手定制化创作.";
+        sCell.tit4.text=@"交付用户";
+        sCell.lab4.numberOfLines=0;
+        sCell.lab4.text=@"经理人与客户验收并支付尾款.";
+        
+        [sCell.M1Image setImage:[UIImage imageNamed:@"customnext"]];
+        [sCell.M2Image setImage:[UIImage imageNamed:@"customnext"]];
+        [sCell.M3Image setImage:[UIImage imageNamed:@"customnext"]];
+        
+        
         return sCell;
-
+    }else if (indexPath.section==2) {
+        SpecialDiscuss *sCell = (SpecialDiscuss *)[tableView dequeueReusableCellWithIdentifier:@"SpecialDiscuss" forIndexPath:indexPath];
+        [sCell.L1Image setImage:[UIImage imageNamed:@"custom5"]];
+        [sCell.L2Image setImage:[UIImage imageNamed:@"custom6"]];
+        [sCell.L3Image setImage:[UIImage imageNamed:@"custom3"]];
+        [sCell.L4Image setImage:[UIImage imageNamed:@"custom4"]];
+        
+        sCell.tit1.text=@"定制需求";
+        sCell.lab1.numberOfLines=0;
+        sCell.lab1.text=@"与销售经理人沟通,对易购500平台未提供的艺术品提出所购期望.";
+        sCell.tit2.text=@"需求确认";
+        sCell.lab2.numberOfLines=0;
+        sCell.lab2.text=@"销售经理人与艺术家确认创作意向,确定创作风格,材质规格等创作细节.";
+        sCell.tit3.text=@"定制化创作";
+        sCell.lab3.numberOfLines=0;
+        sCell.lab3.text=@"根据客户需求,艺术家着手定制化创作.";
+        sCell.tit4.text=@"交付用户";
+        sCell.lab4.numberOfLines=0;
+        sCell.lab4.text=@"经理人与客户验收并支付尾款.";
+        
+        [sCell.M1Image setImage:[UIImage imageNamed:@"customnext"]];
+        [sCell.M2Image setImage:[UIImage imageNamed:@"customnext"]];
+        [sCell.M3Image setImage:[UIImage imageNamed:@"customnext"]];
+        
+        return sCell;
     }else{
          UITableViewCell *cell = [[UITableViewCell alloc ]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"managerCell" ];
         return cell;
