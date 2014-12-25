5bd44ff61ca10194b8e6d87f89ee697c4561fb55
emccchina <wuhaizeng@emcc.net.cn>
2014-12-25 10:49:52 +0800

    back button

---
+++ ZYYG/Common/PayManager.h
@@ -0,0 +1,13 @@
+//
+//  PayManager.h
+//  ZYYG
+//
+//  Created by EMCC on 14/12/25.
+//  Copyright (c) 2014年 wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PayManager : NSObject
+
+@end
---
+++ ZYYG/Common/PayManager.m
@@ -0,0 +1,15 @@
+//
+//  PayManager.m
+//  ZYYG
+//
+//  Created by EMCC on 14/12/25.
+//  Copyright (c) 2014年 wu. All rights reserved.
+//
+
+#import "PayManager.h"
+
+@implementation PayManager
+
+
+
+@end
