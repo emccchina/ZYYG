818b1e259b5647312832cae70b8e01c9fc62377d
emccchina <wuhaizeng@emcc.net.cn>
2015-02-06 17:10:09 +0800

    倒计时

--- ZYYG/ViewControllers/RegisterVC.m
+++ ZYYG/ViewControllers/RegisterVC.m
@@ -33,7 +33,7 @@
     // Do any additional setup after loading the view.
     [self showBackItem];
     [self setInitState];
-    
+    [self startTime];
     self.userNameTF.delegate = self;
     self.verifyTF.delegate = self;
     self.passwordTF.delegate = self;
@@ -96,7 +96,7 @@
         [self showAlertView:@"请填写手机号码"];
         return;
     }
-
+    [self startTime];
     [self showIndicatorView:kNetworkConnecting];
     AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
     manager.responseSerializer = [AFHTTPResponseSerializer serializer];
@@ -122,6 +122,37 @@
         [self showAlertView:kNetworkNotConnect];
     }];
 
+}
+
+-(void)startTime{
+    __block int timeout=30; //倒计时时间
+    dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+    dispatch_source_t _timer = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0,queue);
+    dispatch_source_set_timer(_timer,dispatch_walltime(NULL, 0),1.0*NSEC_PER_SEC, 0); //每秒执行
+    dispatch_source_set_event_handler(_timer, ^{
+        if(timeout<=0){ //倒计时结束，关闭
+            dispatch_source_cancel(_timer);
+            dispatch_async(dispatch_get_main_queue(), ^{
+                //设置界面的按钮显示 根据自己需求设置
+                [self.getVerifiBut setTitle:@"发送验证码" forState:UIControlStateNormal];
+                self.getVerifiBut.userInteractionEnabled = YES;
+            });
+        }else{
+            //            int minutes = timeout / 60;
+            int seconds = timeout % 60;
+            NSString *strTime = [NSString stringWithFormat:@"%.2d", seconds];
+            dispatch_async(dispatch_get_main_queue(), ^{
+                //设置界面的按钮显示 根据自己需求设置
+                [self.getVerifiBut setTitle:[NSString stringWithFormat:@"%@秒后重新发送",strTime] forState:UIControlStateNormal];
+                self.getVerifiBut.userInteractionEnabled = NO;
+                
+            });
+            timeout--;
+            
+        }
+    });
+    dispatch_resume(_timer);
+    
 }
 
 - (void)requestForNewPassword
