265909a089e4c4e2bdb083fd766b49724d865323
emccchina <wuhaizeng@emcc.net.cn>
2015-03-26 10:00:11 +0800

    Server

--- ZYYG/Common/ServerFile.h
+++ ZYYG/Common/ServerFile.h
@@ -9,10 +9,7 @@
 #ifndef ZYYG_ServerFile_h
 #define ZYYG_ServerFile_h
 
-//#define kServerDomain                       @"http://test.zaixianchuangxin.com/mobile/"
-//#define kReceiveURLForPay                   @"http://test.zaixianchuangxin.com/pay/receive.aspx"
 
-#define kServerDomain                       @"http://www.chinart500.com/mobile/"
-#define kReceiveURLForPay                   @"http://www.chinart500.com/pay/receive.aspx"
+
 
 #endif
